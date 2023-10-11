a5812fb9bcc3262c98baef7b174f4f0c309950ae
Evaisa <evagiacosa1@gmail.com>
2023-10-11 17:08:34 +0200

    random sync attempt 1

--- evaisa.arena/files/scripts/append/action_fix.lua
+++ evaisa.arena/files/scripts/append/action_fix.lua
@@ -6,7 +6,7 @@ local remove_list = {
 }
 
 local hook_list = {
-    RANDOM_SPELL = function(orig, ...)
+    --[[RANDOM_SPELL = function(orig, ...)
         local oldSetRandomSeed = SetRandomSeed
         SetRandomSeed = function() 
     
@@ -30,12 +30,12 @@ local hook_list = {
             oldSetRandomSeed(seed, seed)
         end
     
-        print("Got random: "..tostring(Random(0, 100)))
+        GamePrint("Got random: "..tostring(Random(0, 100)))
 
         orig(...)
 
         SetRandomSeed = oldSetRandomSeed
-    end
+    end]]
 }
 
 -- loop backwards through perk_list so we can remove entries
@@ -44,11 +44,40 @@ for i=#actions,1,-1 do
     if remove_list[action.id] then
         table.remove(actions, i)
     else
-        if hook_list[action.id] then
+        local func = action.action
+        action.action = function(...)
+            local oldSetRandomSeed = SetRandomSeed
+            SetRandomSeed = function() 
+        
+                local shooter = EntityGetRootEntity(GetUpdatedEntityID())
+        
+                --GamePrint(EntityGetName(shooter))
+        
+                --oldSetRandomSeed(GameGetFrameNum(), GameGetFrameNum())
+        
+                local seed = 0
+                if(EntityHasTag(shooter, "client"))then
+                    --GamePrint("2: shooter_rng_"..EntityGetName(shooter))
+                    seed = tonumber(GlobalsGetValue("shooter_rng_"..EntityGetName(shooter), "0")) or 0
+                elseif(EntityHasTag(shooter, "player_unit"))then
+                    seed = tonumber(GlobalsGetValue("player_rng", "0"))
+                end
+        
+                GamePrint("Seed forced to: "..tostring(seed))
+        
+                oldSetRandomSeed(seed, seed)
+            end
+
+            func(...)
+
+            SetRandomSeed = oldSetRandomSeed
+        end
+
+        --[[if hook_list[action.id] then
             local func = action.action
             action.action = function(...)
-                return hook_list[action.id](func, ...)
+                hook_list[action.id](func, ...)
             end
-        end
+        end]]
     end
 end
