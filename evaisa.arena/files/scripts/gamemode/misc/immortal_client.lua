171362e291bb25803dde03cf0d0c7ecbd1242212
Evaisa <evagiacosa1@gmail.com>
2023-10-25 22:33:13 +0200

    add prints

--- evaisa.arena/files/scripts/gamemode/misc/immortal_client.lua
+++ evaisa.arena/files/scripts/gamemode/misc/immortal_client.lua
@@ -1,4 +1,6 @@
 function damage_about_to_be_received( damage, x, y, entity_thats_responsible, critical_hit_chance )
+    print("Responsible: "..tostring(entity_thats_responsible))
+    print("damage: "..tostring(damage))
     local is_dummy = nil
     if(entity_thats_responsible ~= 0 and EntityGetName(entity_thats_responsible) == "dummy_damage")then
         is_dummy = true
@@ -6,6 +8,8 @@ function damage_about_to_be_received( damage, x, y, entity_thats_responsible, cr
     if(not is_dummy and entity_thats_responsible ~= GameGetWorldStateEntity())then
         return 0, 0
     end
+
+    print("Returning original damage")
 
     return damage, 0
 end
