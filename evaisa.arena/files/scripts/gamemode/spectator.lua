cb48d71b67891d85406499f16406c6b42a34de03
Evaisa <evagiacosa1@gmail.com>
2024-07-02 22:48:57 +0200

    Fix spectator crash

--- evaisa.arena/files/scripts/gamemode/spectator.lua
+++ evaisa.arena/files/scripts/gamemode/spectator.lua
@@ -166,7 +166,9 @@ SpectatorMode = {
             data.last_selected_perk_string = perk_string
 
 
-            -- handle wand uses remaining
+
+            -- ONE OF THE WORST THINGS I HAVE EVER WRITTEN!!! LETS GOOOOOO
+            -- CURSE OF RA 𓀀 𓀁 𓀂 𓀃 𓀄 𓀅 𓀆 𓀇 𓀈 𓀉 𓀊 𓀋 𓀌 𓀍 𓀎 𓀏 𓀐 𓀑 𓀒 𓀓 𓀔 𓀕 𓀖 𓀗 𓀘 𓀙 𓀚 𓀛 𓀜 𓀝 𓀞 𓀟 𓀠 𓀡 𓀢 𓀣 𓀤 𓀥 𓀦 𓀧 𓀨 𓀩 𓀪 𓀫 𓀬 𓀭 𓀮 𓀯 𓀰 𓀱 𓀲 𓀳 𓀴 𓀵 𓀶 𓀷 𓀸 𓀹 𓀺 𓀻 𓀼 𓀽 𓀾 𓀿 𓁀 𓁁 𓁂 𓁃 𓁄 𓁅 𓁆 𓁇 𓁈 𓁉 𓁊 𓁋 𓁌 𓁍 𓁎 𓁏 𓁐 𓁑 𓀄 𓀅 𓀆 𓀇 𓀈 𓀉 𓀊
             local items = GameGetAllInventoryItems(player.entity)
             local spectator_items = GameGetAllInventoryItems(data.spectator_entity)
             -- find spells
