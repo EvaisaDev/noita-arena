64d5eb40246cb5748cee675260caa9ad8e119056
Evaisa <evagiacosa1@gmail.com>
2026-03-20 17:25:05 +0100

    Update main.lua

--- evaisa.arena/files/scripts/gamemode/main.lua
+++ evaisa.arena/files/scripts/gamemode/main.lua
@@ -712,6 +712,7 @@ np.SetGameModeDeterministic(true)
 ArenaMode = {
     id = "arena",
     name = "$arena_gamemode_name",
+    version = 224,
     version_display = function(version_string)
         return version_string .. " - " .. tostring(content_hash)
     end,
